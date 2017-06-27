9a75ef995d70bafdde924198e0deaa5d876162ab
ibrahim edib kokdemir <kokdemir@gmail.com>
2017-06-27 23:04:20 +0300

    Update pg_stat_activity.py

--- examples/pg_stat_activity.py
+++ examples/pg_stat_activity.py
@@ -4,7 +4,6 @@ from mamonsu.lib.plugin import Plugin
 from mamonsu.plugins.pgsql.pool import Pooler
 
 
-# Считаем нагрузку по количеству выполняемых запросов в БД
 class SparkmesActivity(Plugin):
     # execute method run() every 60s
     Interval = 60
@@ -12,42 +11,42 @@ class SparkmesActivity(Plugin):
     # key, zbx_key, description, units, delta, field_position  value_type
     #   0      1         2         3      4          5             6
     Items = [
-        ('calls_upd', 'pgsql.queries.sparkmes', 'число запросов (modify)', 'n/m',
+        ('calls_upd', 'pgsql.queries.sparkmes', 'number of queries (modify)', 'n/m',
          Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
-        ('calls_sel', 'pgsql.queries.sparkmes', 'число запросов (select)', 'n/m',
-         Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
-
-        ('time_upd', 'pgsql.queries.sparkmes', 'время (modify)', Plugin.UNITS.ms,
-         Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
-        ('time_sel', 'pgsql.queries.sparkmes', 'время (select)', Plugin.UNITS.ms,
-         Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
-
-        ('callsfirm', 'pgsql.queries.sparkmes', 'число запросов (firm)', 'n/m',
-         Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
-        ('callsip_s', 'pgsql.queries.sparkmes', 'число запросов (ip)', 'n/m',
+        ('calls_sel', 'pgsql.queries.sparkmes', 'number of requests (select)', 'n/m',
          Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
 
-        ('timefirm', 'pgsql.queries.sparkmes', 'время (firm)', Plugin.UNITS.ms,
+        ('time_upd', 'pgsql.queries.sparkmes', 'time (modify)', Plugin.UNITS.ms,
          Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
-        ('timeip_s', 'pgsql.queries.sparkmes', 'время (ip)', Plugin.UNITS.ms,
+        ('time_sel', 'pgsql.queries.sparkmes', 'time (select)', Plugin.UNITS.ms,
+         Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
+
+        ('callsfirm', 'pgsql.queries.sparkmes', 'number of requests (firm)', 'n/m',
+         Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
+        ('callsip_s', 'pgsql.queries.sparkmes', 'number of requests (ip)', 'n/m',
+         Plugin.DELTA.simple_change, 1, Plugin.VALUE_TYPE.numeric_unsigned),
+
+        ('timefirm', 'pgsql.queries.sparkmes', 'time (firm)', Plugin.UNITS.ms,
+         Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
+        ('timeip_s', 'pgsql.queries.sparkmes', 'time (ip)', Plugin.UNITS.ms,
          Plugin.DELTA.simple_change, 2, Plugin.VALUE_TYPE.numeric_float),
     ]
     # name, type, ('graph item', color, yaxisside, drawtype)
     #   0     1         2.0        2.1     2.2       2.3
     Graphs = [
-        ('sparkmes активность (calls)', 0, (('calls_upd', 'CCFFCC', 1, 5),
+        ('sparkmes activity (calls)', 0, (('calls_upd', 'CCFFCC', 1, 5),
                                             ('calls_sel', 'CCFFFF', 1, 5),
                                             ('calls_upd_Yprev', '009900', 1, 2),
                                             ('calls_sel_Yprev', '009999', 1, 2),),),
-        ('sparkmes активность (time)', 0, (('time_upd', 'CCFFCC', 1, 5),
+        ('sparkmes activity (time)', 0, (('time_upd', 'CCFFCC', 1, 5),
                                            ('time_sel', 'CCFFFF', 1, 5),
                                            ('time_upd_Yprev', '009900', 1, 2),
                                            ('time_sel_Yprev', '009999', 1, 2),),),
-        ('sparkmes поиск (calls)', 0, (('callsfirm', 'CCFFCC', 1, 5),
+        ('sparkmes search (calls)', 0, (('callsfirm', 'CCFFCC', 1, 5),
                                        ('callsip_s', 'CCFFFF', 0, 5),
                                        ('callsfirm_Yprev', '009900', 1, 2),
                                        ('callsip_s_Yprev', '009999', 0, 2),),),
-        ('sparkmes поиск (time)', 0, (('timefirm', 'CCFFCC', 1, 5),
+        ('sparkmes search (time)', 0, (('timefirm', 'CCFFCC', 1, 5),
                                       ('timeip_s', 'CCFFFF', 0, 5),
                                       ('timefirm_Yprev', '009900', 1, 2),
                                       ('timeip_s_Yprev', '009999', 0, 2),),),
